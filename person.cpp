a1ed0c1f561e487d29da8a3fdefe777caea13877
Shynih <jorgepeng2001@gmail.com>
2020-10-14 21:03:39 -0700

    Fixed array onto array cpying not having same size

--- person.cpp
+++ person.cpp
@@ -6,7 +6,7 @@ using std::cout;
 using std::endl;
 
 Person::Person(char *name, Person* father, Person* mother){
-    this->name = new char[strlen(name)];
+    this->name = new char[strlen(name) + 1];
     strcpy(this->name, name);
     this->father = father;
     this->mother = mother;
